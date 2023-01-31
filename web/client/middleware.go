c4d8db57a20f8827964a5565996128f80546099b
Alejandro Mery <amery@jpi.io>
2023-01-31 17:15:52 +0000

    web/client: add Middleware() returning the Middleware for client files
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/client/middleware.go
+++ web/client/middleware.go
@@ -1,3 +1,24 @@
 package client
 
 //go:generate go run go.sancus.dev/web/cmd/embed -n embedded -o files.go dist
+
+import (
+	"log"
+
+	"go.sancus.dev/web"
+	"go.sancus.dev/web/fs"
+)
+
+func Middleware() web.MiddlewareHandlerFunc {
+	fsys, err := fs.Sub(&embedded, "dist")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	overlay := fs.FS{
+		FS:    fsys,
+		Index: "index.html",
+	}
+
+	return overlay.Middleware
+}
