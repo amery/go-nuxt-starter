5c98bb6fe88389ba9b07687e3f7571ab97c1189e
Alejandro Mery <amery@jpi.io>
2023-01-31 19:08:09 +0000

    apps/web: enable @kevinmarrec/nuxt-pwa
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- src/apps/web/nuxt.config.ts
+++ src/apps/web/nuxt.config.ts
@@ -3,7 +3,16 @@ export default defineNuxtConfig({
 	srcDir: "src",
 	telemetry: true,
 
+	modules: [
+		// https://github.com/kevinmarrec/nuxt-pwa-module
+		"@kevinmarrec/nuxt-pwa",
+	],
+
 	typescript: {
 		strict: true,
+	},
+
+	pwa: {
+		icon: false,
 	},
 })
