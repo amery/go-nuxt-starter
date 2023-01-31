89871b0447ab0ed31662660dd5cdb2f4c62f083d
Alejandro Mery <amery@jpi.io>
2023-01-31 18:56:23 +0000

    web/client: add `/api` proxy to the server it's embedded on
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/client/nuxt.config.ts
+++ web/client/nuxt.config.ts
@@ -1,9 +1,23 @@
+// API backend
+const apiPrefix = "/api"
+const apiBackendURL = process.env?.BACKEND || "http://127.0.0.1:8080";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	srcDir: "src",
 	telemetry: true,
 
+	modules: [
+		"@nuxt-alt/proxy",
+	],
+
 	typescript: {
 		strict: true,
+	},
+
+	proxy: {
+		proxies: {
+			[apiPrefix]: apiBackendURL,
+		},
 	},
 });
